dae0e4405145a2f73b755582cd4051d2b937d7b6
srevrtt <everettsistrunk@icloud.com>
2023-11-09 21:06:59 -0700

    added glad

--- debug-build.sh
+++ debug-build.sh
@@ -1,11 +1,16 @@
 
+INCFLAGS=-Iexternal
+
 printf "Building in DEBUG MODE...\n\n"
 
 printf "src/main.c\n"
-gcc -c src/main.c -o objs/main.o
+gcc -c src/main.c -o objs/main.o $INCFLAGS
 
 printf "src/linux_window.c\n"
-gcc -c src/linux_window.c -o objs/linux_window.o
+gcc -c src/linux_window.c -o objs/linux_window.o $INCFLAGS
+
+printf "external/glad/glad.c\n"
+gcc -c external/glad/glad.c -o objs/glad.o $INCFLAGS
 
 printf "build/ped_DEBUG\n"
-gcc objs/*.o -o build/ped_DEBUG -lX11
+gcc objs/*.o -o build/ped_DEBUG -lX11 -ldl
